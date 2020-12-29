64fd74b707a7117041659860bdf6f9c032cbdb47
Z-Kamili <zakariakamili97@gmail.com>
2020-12-29 16:37:05 +0100

    my files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,1 @@
+<h1> Welcom   {{auth()->user()->name}} </h1>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Upwork</title>
+    </head>
+    <body>
+      <div class="container mx-auto px-4">
+      @yield('content')
+      </div>
+    </body>
+</html>
